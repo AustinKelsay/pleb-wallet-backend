3871a9d810746c1326c5bed546c5d92a711ef01d
austinkelsay <austinkelsay@yahoo.com>
2023-08-19 17:38:58 -0500

    Changing /test/trest to /test

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 // GET all users
 // Before processing the request, we apply the 'authenticateAdmin' middleware to protect the ability to see all users
-router.get("/test", (req, res) => {
+router.get("/", (req, res) => {
   // Call the 'findAll' method from our User model. This method retrieves all user records from the database.
   res.status(200).json({ message: "Hello!" });
 });

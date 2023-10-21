20b425e66aecb49eaa7a75d1787d8263634a848c
austinkelsay <austinkelsay@yahoo.com>
2023-10-21 12:31:17 -0500

    Increase rate limiting to 1000 per 15 mins

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ server.use(cors());
 server.use(
   rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
+    max: 1000, // limit each IP to 100 requests per windowMs
   })
 );
 
